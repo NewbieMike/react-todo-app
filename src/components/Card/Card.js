6972b4d95ff66b5c9b2a68a4783a18a6469adee8
Michał Micherda <mmicherdakr@gmail.com>
2021-04-18 11:00:28 +0200

    Add creator

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './Card.scss';
 import PropTypes from 'prop-types';
-
+import ReactHtmlParser from 'react-html-parser';
 const Card = props => (
     <div className={styles.component}>
-        <p className={props.titleText}>{ReactHtmlParser(props.titleText)}</p>
+        <p className={props.title}>{ReactHtmlParser(props.title)}</p>
     </div>
     )
     Card.propTypes = {
-        titleText: PropTypes.string,
+        title: PropTypes.string,
       };
 
 export default Card;
